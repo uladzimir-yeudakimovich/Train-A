4578db929c94b618a6500bf1e37bd162d11809e0
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-08-14 23:12:49 +0500

    feat: add titles for routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,11 +18,13 @@ export enum RoutePath {
 export const routes: Routes = [
   {
     path: RoutePath.Search,
+    title: 'Search',
     loadComponent: () =>
       import('./pages/search-page/search-page.component').then((m) => m.SearchPageComponent),
   },
   {
     path: RoutePath.Registration,
+    title: 'Sign Up',
     loadComponent: () =>
       import('./pages/registration-page/registration-page.component').then(
         (m) => m.RegistrationPageComponent,
@@ -30,11 +32,13 @@ export const routes: Routes = [
   },
   {
     path: RoutePath.Login,
+    title: 'Sign In',
     loadComponent: () =>
       import('./pages/login-page/login-page.component').then((m) => m.LoginPageComponent),
   },
   {
     path: RoutePath.UserProfile,
+    title: 'Profile',
     loadComponent: () =>
       import('./pages/user-profile-page/user-profile-page.component').then(
         (m) => m.UserProfilePageComponent,
@@ -42,6 +46,7 @@ export const routes: Routes = [
   },
   {
     path: RoutePath.TripDetails,
+    title: 'Trip Details',
     loadComponent: () =>
       import('./pages/trip-details-page/trip-details-page.component').then(
         (m) => m.TripDetailsPageComponent,
@@ -49,16 +54,19 @@ export const routes: Routes = [
   },
   {
     path: RoutePath.Orders,
+    title: 'Orders',
     loadComponent: () =>
       import('./pages/order-page/order-page.component').then((m) => m.OrderPageComponent),
   },
   {
     path: RoutePath.Admin,
+    title: 'Admin',
     loadComponent: () =>
       import('./pages/admin-page/admin-page.component').then((m) => m.AdminPageComponent),
     children: [
       {
         path: RoutePath.AdminStations,
+        title: 'Admin Stations',
         loadComponent: () =>
           import('./pages/stations-page/stations-page.component').then(
             (m) => m.StationsPageComponent,
@@ -66,6 +74,7 @@ export const routes: Routes = [
       },
       {
         path: RoutePath.AdminCarriages,
+        title: 'Admin Carriages',
         loadComponent: () =>
           import('./pages/carriages-page/carriages-page.component').then(
             (m) => m.CarriagesPageComponent,
@@ -73,11 +82,13 @@ export const routes: Routes = [
       },
       {
         path: RoutePath.AdminRoutes,
+        title: 'Admin Routes',
         loadComponent: () =>
           import('./pages/routes-page/routes-page.component').then((m) => m.RoutesPageComponent),
       },
       {
         path: RoutePath.AdminRideManagement,
+        title: 'Admin Ride Management',
         loadComponent: () =>
           import('./pages/ride-management-page/ride-management-page.component').then(
             (m) => m.RideManagementPageComponent,
@@ -87,6 +98,7 @@ export const routes: Routes = [
   },
   {
     path: RoutePath.NotFound,
+    title: 'Not Found',
     loadComponent: () =>
       import('./pages/not-found-page/not-found-page.component').then(
         (m) => m.NotFoundPageComponent,
