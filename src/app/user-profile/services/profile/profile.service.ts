3bbf0baf1e282c8fd0a3728178fa2fdb5aad7d1a
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-08-15 20:32:43 +0500

    feat: add methods to profile service

--- src/app/user-profile/services/profile/profile.service.ts
+++ src/app/user-profile/services/profile/profile.service.ts
@@ -1,6 +1,74 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, tap, Observable, throwError } from 'rxjs';
+
+import {
+  UpdateInformationRequestBody,
+  UpdatePasswordRequestBody,
+  UserProfileResponse,
+} from '../../interfaces';
+
+// TODO: replace by auth service
+const FAKE_CREDENTIALS = {
+  email: 'cc@cc.com',
+  password: 'dddddddd',
+};
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProfileService {}
+export class ProfileService {
+  constructor(private readonly http: HttpClient) {}
+
+  // TODO: replace by auth service
+  public signUp(): Observable<object> {
+    return this.http.post('/api/signup', FAKE_CREDENTIALS);
+  }
+
+  // TODO: replace by auth service
+  public signIn(): void {
+    this.http
+      .post('/api/signin', FAKE_CREDENTIALS)
+      .pipe(
+        tap((response) => {
+          // window.localStorage.setItem('token', response.token);
+          console.log('FROM TAP', response);
+        }),
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  public getUserInformation(token: string): Observable<UserProfileResponse> {
+    return this.http
+      .get<UserProfileResponse>('/api/profile', {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .pipe(catchError((error) => throwError(() => error)));
+  }
+
+  public updateUserInformation(token: string, body: UpdateInformationRequestBody) {
+    return this.http
+      .put('/api/profile', body, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .pipe(catchError((error) => throwError(() => error)));
+  }
+
+  public updateUserPassword(token: string, body: UpdatePasswordRequestBody) {
+    return this.http
+      .put('/api/profile/password', body, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .pipe(catchError((error) => throwError(() => error)));
+  }
+
+  public logout(token: string) {
+    return this.http
+      .delete('/api/logout', {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .pipe(catchError((error) => throwError(() => error)));
+  }
+}
