219134d0b26157de8d8c821a2aa0afa438652de4
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2024-08-17 00:47:24 +0200

    refactor: move login

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,17 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { RegistrationCredentials } from '../models/auth.model';
+import { LoginCredentials, RegistrationCredentials, TokenInterface } from '../models/auth.model';
+import { RoutePath } from '@shared/models/enums/route-path.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient) { }
 
   registration(credentials: RegistrationCredentials): Observable<object> {
-    return this.http.post<object>('signup', credentials);
+    return this.http.post<object>(RoutePath.Registration, credentials);
+  }
+
+  login(credentials: LoginCredentials): Observable<TokenInterface> {
+    return this.http.post<TokenInterface>(RoutePath.Login, credentials);
   }
 }
