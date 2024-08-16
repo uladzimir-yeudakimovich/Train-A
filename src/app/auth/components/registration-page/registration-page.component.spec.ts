b8c17429f7764a6fbab4500272fa9d73eb05fd92
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2024-08-16 12:52:40 +0200

    feat: add tests

--- src/app/auth/components/registration-page/registration-page.component.spec.ts
+++ src/app/auth/components/registration-page/registration-page.component.spec.ts
@@ -1,6 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+import { ChangeDetectorRef } from '@angular/core';
 
 import { RegistrationPageComponent } from './registration-page.component';
+import { AuthService } from '../../services/auth.service';
+import { RoutePath } from '../../../shared/models/enums/route-path.enum';
+import { authServiceMock, routerMock } from '../../../testing/mock-data';
 
 describe('RegistrationPageComponent', () => {
   let component: RegistrationPageComponent;
@@ -8,7 +19,18 @@ describe('RegistrationPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegistrationPageComponent],
+      imports: [
+        ReactiveFormsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: Router, useValue: routerMock },
+        ChangeDetectorRef
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegistrationPageComponent);
@@ -18,5 +40,18 @@ describe('RegistrationPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to login page on successful registration', () => {
+    component.registrationForm.controls['email'].setValue('test@example.com');
+    component.registrationForm.controls['password'].setValue('password123');
+    component.registrationForm.controls['confirmPassword'].setValue('password123');
+
+    authServiceMock.registration.mockReturnValue(of({}));
+
+    component.onRegistration();
+    fixture.detectChanges();
+
+    expect(routerMock.navigate).toHaveBeenCalledWith([RoutePath.Login]);
   });
 });
