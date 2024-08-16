3945d87132a6d95e8c0d91d10280b8127fbcbabd
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-08-16 15:57:38 +0500

    feat: add functionality for retrieving and updating user information

--- src/app/user-profile/components/user-profile/user-profile.component.ts
+++ src/app/user-profile/components/user-profile/user-profile.component.ts
@@ -1,12 +1,104 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatMiniFabButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatInput } from '@angular/material/input';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { ProfileService } from '../../services';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatIcon,
+    MatMiniFabButton,
+    MatFormField,
+    MatInput,
+    MatProgressSpinner,
+  ],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss'
+  styleUrl: './user-profile.component.scss',
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  private readonly profileService = inject(ProfileService);
 
+  private readonly formBuilder = inject(NonNullableFormBuilder);
+
+  private readonly destroyRef = inject(DestroyRef);
+
+  public readonly userInformationForm = this.formBuilder.group({
+    email: this.formBuilder.control(''),
+    name: this.formBuilder.control(''),
+  });
+
+  public readonly editMode = signal({
+    email: false,
+    name: false,
+  });
+
+  public readonly loading = signal(false);
+
+  public toggleEditMode(field: 'name' | 'email'): void {
+    if (field === 'name') {
+      this.editMode.update((value) => ({ ...value, name: !value.name }));
+    }
+
+    if (field === 'email') {
+      this.editMode.update((value) => ({ ...value, email: !value.email }));
+    }
+  }
+
+  public ngOnInit(): void {
+    this.getUserInformation();
+  }
+
+  public onSubmit(): void {
+    this.editMode.set({ email: false, name: false });
+    this.updateUserInformation();
+  }
+
+  private getUserInformation(): void {
+    this.showSpinner();
+
+    this.profileService
+      .getUserInformation()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: ({ name, email }) => {
+          this.userInformationForm.setValue({ name, email });
+        },
+        error: () => {
+          this.hideSpinner();
+        },
+        complete: () => {
+          this.hideSpinner();
+        },
+      });
+  }
+
+  private updateUserInformation(): void {
+    this.showSpinner();
+
+    this.profileService
+      .updateUserInformation(this.userInformationForm.getRawValue())
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        error: () => {
+          this.hideSpinner();
+        },
+        complete: () => {
+          this.hideSpinner();
+        },
+      });
+  }
+
+  private showSpinner(): void {
+    this.loading.set(true);
+  }
+
+  private hideSpinner(): void {
+    this.loading.set(false);
+  }
 }
