6ad6710f0b23233d359195a0c82013a8f487865c
Yauheni Basalai <yauheni.basalai@gmail.com>
2024-08-16 20:49:25 +0300

    fix: add aliases and update navigation

--- src/app/auth/services/login.service.ts
+++ src/app/auth/services/login.service.ts
@@ -1,7 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { LoginFormInterface } from '../models/login-form.interface';
-import { LoginTokenInterface } from '../models/login-token.interface';
+import { RoutePath } from '@shared/models/enums/route-path.enum';
+import { LoginFormInterface } from '@auth/models/login-form.interface';
+import { LoginTokenInterface } from '@auth/models/login-token.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,6 @@ export class LoginService {
   private readonly http = inject(HttpClient);
 
   login(body: LoginFormInterface) {
-    return this.http.post<LoginTokenInterface>('/api/signin', body);
+    return this.http.post<LoginTokenInterface>(`/api/${RoutePath.Login}`, body);
   }
 }
