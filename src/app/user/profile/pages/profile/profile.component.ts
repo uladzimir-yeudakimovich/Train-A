e6f8c7ec8c8f955e0fc29d42e7a1e870fe331de4
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-08-17 16:21:02 +0500

    fix: add content to profile page

--- src/app/user/profile/pages/profile/profile.component.ts
+++ src/app/user/profile/pages/profile/profile.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { UserProfileComponent } from '@user/profile/components/user-profile/user-profile.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [UserProfileComponent],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {
-
-}
+export class ProfileComponent {}
