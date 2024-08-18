5777a073580acf8feac24ad62526c8eccfd84330
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-08-18 14:02:16 +0500

    refactor: use ApiEndpoint enum

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApiEndpoint } from '@shared/models/enums/api-endpoint.enum';
 import { Observable, tap } from 'rxjs';
 import { Credentials, Token } from '../models/auth.model';
-import { RoutePath } from '@shared/models/enums/route-path.enum';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -12,23 +12,22 @@ const httpOptions = {
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   registration(credentials: Credentials): Observable<object> {
-    return this.http.post<object>(RoutePath.Registration, credentials, httpOptions);
+    return this.http.post<object>(ApiEndpoint.SignUp, credentials, httpOptions);
   }
 
   login(credentials: Credentials): Observable<Token> {
     const { email } = credentials;
-    return this.http.post<Token>(RoutePath.Login, credentials, httpOptions).pipe(
-      tap(({token}) => {
+    return this.http.post<Token>(ApiEndpoint.SignIn, credentials, httpOptions).pipe(
+      tap(({ token }) => {
         localStorage.setItem('token', token);
         localStorage.setItem('username', email);
-      })
+      }),
     );
   }
 
--- src/app/user/profile/services/profile.service.ts
+++ src/app/user/profile/services/profile.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, throwError } from 'rxjs';
 import { Password, UserInfo, UserRole } from '@auth/models/auth.model';
+import { ApiEndpoint } from '@shared/models/enums/api-endpoint.enum';
+import { catchError, Observable, throwError } from 'rxjs';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -16,20 +17,20 @@ export class ProfileService {
   constructor(private readonly http: HttpClient) {}
 
   getUserInformation(): Observable<UserRole> {
-    return this.http.get<UserRole>('profile', httpOptions).pipe(
-      catchError((error) => throwError(() => error))
-    );
+    return this.http
+      .get<UserRole>(ApiEndpoint.Profile, httpOptions)
+      .pipe(catchError((error) => throwError(() => error)));
   }
 
   updateUserInformation(body: UserInfo): Observable<object> {
-    return this.http.put('profile', body, httpOptions).pipe(
-      catchError((error) => throwError(() => error))
-    );
+    return this.http
+      .put(ApiEndpoint.Profile, body, httpOptions)
+      .pipe(catchError((error) => throwError(() => error)));
   }
 
   updateUserPassword(body: Password): Observable<object> {
-    return this.http.put('profile/password', body, httpOptions).pipe(
-      catchError((error) => throwError(() => error))
-    );
+    return this.http
+      .put(ApiEndpoint.ProfilePassword, body, httpOptions)
+      .pipe(catchError((error) => throwError(() => error)));
   }
 }
