f31a6f471f6ebe8f1d0460ef1aec68d196a3f91c
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-08-18 13:47:08 +0500

    feat: add ApiEndpoint enum

---
+++ src/app/shared/models/enums/api-endpoint.enum.ts
@@ -0,0 +1,14 @@
+// DO NOT DELETE THIS!!!!!!!!!!!!!!!!
+export const enum ApiEndpoint {
+  SignUp = 'signup',
+  SignIn = 'signin',
+  Logout = 'logout',
+  Profile = 'profile',
+  ProfilePassword = 'profile/password',
+  Search = 'search',
+  Order = 'order',
+  Users = 'users',
+  Station = 'station',
+  Carriage = 'carriage',
+  Route = 'route',
+}
