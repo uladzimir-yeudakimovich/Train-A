01a84d738fc19c81ab737108a90e832d78eec46a
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-08-19 18:31:28 +0500

    feat: create ride list and ride card components

---
+++ src/app/admin/components/ride-card/ride-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ride-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './ride-card.component.html',
+  styleUrl: './ride-card.component.scss'
+})
+export class RideCardComponent {
+
+}
