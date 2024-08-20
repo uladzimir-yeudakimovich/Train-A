5dba40ad51aea5fb1c195a1231552b860adec31d
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-08-20 18:32:39 +0500

    feat: setup rides page

--- src/app/admin/pages/rides/rides.component.ts
+++ src/app/admin/pages/rides/rides.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { RideListComponent } from '@admin/components/ride-list/ride-list.component';
+import { Component, OnInit, signal } from '@angular/core';
+import { MatButton, MatMiniFabButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-rides',
   standalone: true,
-  imports: [],
+  imports: [MatButton, MatMiniFabButton, MatIcon, RideListComponent],
   templateUrl: './rides.component.html',
   styleUrl: './rides.component.scss',
 })
-export class RidesComponent {}
+export class RidesComponent implements OnInit {
+  routeId = signal('0');
+
+  constructor(private readonly activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.routeId.set(params.get('id') ?? '0');
+    });
+  }
+}
