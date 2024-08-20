5e5dd9c8f5bfd2faa16ca2370456e5cef5994a4d
Ruslan Rabadanov <rabadanov.ruslann@gmail.com>
2024-08-20 11:47:57 +0200

    feat: create Route form component; update Route card display

--- src/app/admin/components/route-card/route-card.component.ts
+++ src/app/admin/components/route-card/route-card.component.ts
@@ -1,13 +1,16 @@
 import { RailRoute } from '@admin/models/route.model';
 import { RouteManagementService } from '@admin/services/route-management/route-management.service';
 import { Component, input, OnInit, signal } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RoutePath } from '@shared/models/enums/route-path.enum';
 
 @Component({
   selector: 'app-route-card',
   standalone: true,
-  imports: [MatIcon, MatToolbarRow, MatToolbar],
+  imports: [MatIcon, MatToolbarRow, MatToolbar, MatButton],
   templateUrl: './route-card.component.html',
   styleUrl: './route-card.component.scss',
 })
@@ -15,10 +18,19 @@ export class RouteCardComponent implements OnInit {
   route = input.required<RailRoute>();
   cities = signal<string[]>([]);
 
-  constructor(private routeService: RouteManagementService) {}
+  constructor(
+    private routeService: RouteManagementService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
     console.log('route(): ', this.route());
     this.routeService.getStationCities(this.route().path).then(cities => this.cities.set(cities)); 
+  }
+
+  onAssignRideClick(): void {
+    const routeId = this.route().id;
+    this.router.navigate([RoutePath.AdminRoutes, routeId], { relativeTo: this.activatedRoute });
   }
 }
---
+++ src/app/admin/components/route-form/route-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-route-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './route-form.component.html',
+  styleUrl: './route-form.component.scss'
+})
+export class RouteFormComponent {
+
+}
