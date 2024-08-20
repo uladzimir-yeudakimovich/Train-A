d674feb4db4451a8a9b600842d2465ca972bd9d0
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-08-20 18:39:29 +0500

    feat: add onInit hook to rides store

--- src/app/admin/store/ride.store.ts
+++ src/app/admin/store/ride.store.ts
@@ -1,5 +1,7 @@
 import { RouteInformation } from '@admin/interfaces';
-import { signalStore, withState } from '@ngrx/signals';
+import { AdminService } from '@admin/services/admin.service';
+import { inject } from '@angular/core';
+import { patchState, signalStore, withHooks, withState } from '@ngrx/signals';
 
 export interface RidesState extends RouteInformation {
   loading: boolean;
@@ -12,4 +14,33 @@ const initialState: RidesState = {
   loading: false,
 };
 
-export const RidesStore = signalStore({ providedIn: 'root' }, withState(initialState));
+export const RidesStore = signalStore(
+  { providedIn: 'root' },
+  withState(initialState),
+  withHooks({
+    onInit(store) {
+      const adminService = inject(AdminService);
+
+      patchState(store, { loading: true });
+
+      // TODO: Temporary implementation
+      const ROUTE_ID = 1;
+
+      adminService.getRouteInformation(ROUTE_ID).subscribe({
+        next: (response) => {
+          patchState(store, {
+            routeId: response.routeId,
+            stations: response.stations,
+            schedule: response.schedule,
+          });
+        },
+        error: () => {
+          patchState(store, { loading: false });
+        },
+        complete: () => {
+          patchState(store, { loading: false });
+        },
+      });
+    },
+  }),
+);
