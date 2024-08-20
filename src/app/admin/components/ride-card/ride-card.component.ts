08396b09c1e5b62d7fc4bffc1fb3f34ac2901140
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-08-20 19:53:11 +0500

    feat: add base data and layout for ride list

--- src/app/admin/components/ride-card/ride-card.component.ts
+++ src/app/admin/components/ride-card/ride-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Segment } from '@admin/interfaces';
+import { DatePipe } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-ride-card',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './ride-card.component.html',
   styleUrl: './ride-card.component.scss',
 })
-export class RideCardComponent {}
+export class RideCardComponent {
+  rideId = input.required<number>();
+
+  stations = input.required<string[]>();
+
+  segments = input.required<Segment[]>();
+}
--- src/app/admin/components/ride-list/ride-list.component.ts
+++ src/app/admin/components/ride-list/ride-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { RidesStore } from '@admin/store/ride.store';
+import { Component, inject } from '@angular/core';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { RideCardComponent } from '../ride-card/ride-card.component';
 
 @Component({
   selector: 'app-ride-list',
   standalone: true,
-  imports: [],
+  imports: [RideCardComponent, MatProgressSpinner],
   templateUrl: './ride-list.component.html',
   styleUrl: './ride-list.component.scss',
 })
-export class RideListComponent {}
+export class RideListComponent {
+  store = inject(RidesStore);
+}
