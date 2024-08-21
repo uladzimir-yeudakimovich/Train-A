d6ea65d7a1b8a2c5ce18e7af7b3e8173109169d9
Ruslan Rabadanov <rabadanov.ruslann@gmail.com>
2024-08-21 23:27:08 +0200

    feat: delete route

--- src/app/admin/components/route-card/route-card.component.ts
+++ src/app/admin/components/route-card/route-card.component.ts
@@ -1,13 +1,15 @@
 import { RailRoute } from '@admin/models/route.model';
 import { JoinWithDashPipe } from '@admin/pipes/join-with-dash.pipe';
 import { RouteManagementService } from '@admin/services/route-management/route-management.service';
-import { Component, input, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, input, OnInit, Signal, signal } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RoutePath } from '@shared/models/enums/route-path.enum';
 import { RouteFormComponent } from '../route-form/route-form.component';
+import { RouteStore } from '@admin/store/routes/routes.store';
+import { StationStore } from '@admin/store/stations/stations.store';
 
 @Component({
   selector: 'app-route-card',
@@ -18,8 +20,12 @@ import { RouteFormComponent } from '../route-form/route-form.component';
 })
 export class RouteCardComponent implements OnInit {
   route = input.required<RailRoute>();
-  cities = signal<string[]>([]);
+  cities = computed(() => {
+    return this.routeService.getStationCities(this.route().path)
+  });
   displayUpdateForm = signal<boolean>(false);
+  private routeStore = inject(RouteStore);
+  private stationStore = inject(StationStore);
 
   constructor(
     private routeService: RouteManagementService,
@@ -28,9 +34,11 @@ export class RouteCardComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    console.log('route(): ', this.route());
-    this.routeService.getStationCities(this.route().path)
-      .then(cities => this.cities.set(cities)); 
+    this.stationStore.getStations();
+  }
+
+  onDeleteClick(): void {
+    this.routeStore.deleteRoute(this.route().id);
   }
 
   onAssignRideClick(): void {
