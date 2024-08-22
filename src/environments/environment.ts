1697cda65be7b385ca91734e1f84df339eca969d
Yauheni Basalai <yauheni.basalai@gmail.com>
2024-08-22 16:13:34 +0300

    refactor: remove fucking mapbox

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,1 @@
-export const environment = {
-  mapbox: {
-    accessToken:
-      'pk.eyJ1IjoiZHJhYmFudCIsImEiOiJjbTA1NGtnYjgwNXltMnJzOTZvcjhrNmhyIn0.JAHMUAA6jM733jJL5x-JuQ',
-  },
-};
+export const environment = {};
