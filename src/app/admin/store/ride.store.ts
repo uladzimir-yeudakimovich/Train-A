8bb97fb98dc198ff86ad0fb8ba7355712fc903a2
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-08-22 15:29:30 +0500

    refactor: remove computed field from store

--- src/app/admin/store/ride.store.ts
+++ src/app/admin/store/ride.store.ts
@@ -1,7 +1,7 @@
 import { RouteInformation } from '@admin/interfaces';
 import { AdminService } from '@admin/services/admin.service';
-import { computed, inject } from '@angular/core';
-import { patchState, signalStore, withComputed, withHooks, withState } from '@ngrx/signals';
+import { inject } from '@angular/core';
+import { patchState, signalStore, withHooks, withState } from '@ngrx/signals';
 
 export interface RidesState extends RouteInformation {
   loading: boolean;
@@ -17,9 +17,6 @@ const initialState: RidesState = {
 export const RidesStore = signalStore(
   { providedIn: 'root' },
   withState(initialState),
-  withComputed((state) => ({
-    rides: computed(() => state.schedule().map((ride) => ride)),
-  })),
   withHooks({
     onInit(store) {
       const adminService = inject(AdminService);
