c37388a8eacf4a0a9ccea0c257379f48df4d952b
Yauheni Basalai <yauheni.basalai@gmail.com>
2024-08-22 14:37:46 +0300

    refactor: remove map

--- src/app/admin/pages/stations/components/globalmap/globalmap.component.ts
+++ src/app/admin/pages/stations/components/globalmap/globalmap.component.ts
@@ -1,33 +1,13 @@
 import { StationStore } from '@admin/store/stations.store';
-import { Component, computed, inject } from '@angular/core';
-import { LeafletModule } from '@bluehalo/ngx-leaflet';
-import { latLng, marker, tileLayer } from 'leaflet';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-globalmap',
   standalone: true,
-  imports: [LeafletModule],
+  imports: [],
   templateUrl: './globalmap.component.html',
   styleUrl: './globalmap.component.scss',
 })
 export class GlobalmapComponent {
   private stationStore = inject(StationStore);
-
-  markers = computed(() =>
-    this.stationStore.locations().map((location) => marker(location as [number, number])),
-  );
-
-  options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-      }),
-    ],
-    zoom: 5,
-    center: latLng(46.879966, -121.726909),
-  };
-
-  onClick(event: Event) {
-    console.log(event);
-  }
 }
