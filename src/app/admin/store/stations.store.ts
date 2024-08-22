08d9fdd6267974116a73fb037d1062d570929b5a
Yauheni Basalai <yauheni.basalai@gmail.com>
2024-08-22 23:43:42 +0300

    fix: add stationConfig in store

--- src/app/admin/store/stations.store.ts
+++ src/app/admin/store/stations.store.ts
@@ -1,28 +1,43 @@
 import { computed, inject } from '@angular/core';
 import { signalStore, type, patchState, withMethods, withComputed } from '@ngrx/signals';
-import { withEntities, setAllEntities, removeEntity, addEntity } from '@ngrx/signals/entities';
+import {
+  withEntities,
+  setAllEntities,
+  removeEntity,
+  addEntity,
+  entityConfig,
+} from '@ngrx/signals/entities';
 import { AdminService } from '@admin/services/admin.service';
 import { StationResponseItem } from './../models/station.model';
 import { StationFormData } from '@admin/models/station-form.model';
 
+export const stationConfig = entityConfig({
+  entity: type<StationResponseItem>(),
+  collection: 'stations',
+  selectId: (station) => station.id,
+});
+
 export const StationStore = signalStore(
   { providedIn: 'root' },
-  withEntities({ entity: type<StationResponseItem>(), collection: 'stations' }),
+  withEntities(stationConfig),
 
   withMethods((store, adminService = inject(AdminService)) => ({
     async getStations(): Promise<void> {
+      if (store.stationsEntities.length > 0) {
+        return;
+      }
       const stations = await adminService.getStations();
-      patchState(store, setAllEntities(stations, { collection: 'stations' }));
+      patchState(store, setAllEntities(stations, stationConfig));
     },
 
     async deleteStation(id: number): Promise<void> {
       await adminService.deleteStation(id);
-      patchState(store, removeEntity(id, { collection: 'stations' }));
+      patchState(store, removeEntity(id, stationConfig));
     },
 
     async addStation(body: StationFormData): Promise<void> {
       const station = await adminService.postStation(body);
-      patchState(store, addEntity(station, { collection: 'stations' }));
+      patchState(store, addEntity(station, stationConfig));
     },
   })),
 
