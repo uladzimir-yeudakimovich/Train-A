c1a133b3f32570fc2f029f101404d52425765bce
Ruslan Rabadanov <rabadanov.ruslann@gmail.com>
2024-08-23 14:13:42 +0200

    feat: add spinner; update Route form

--- src/app/admin/pages/routes/route-list/route-list.component.ts
+++ src/app/admin/pages/routes/route-list/route-list.component.ts
@@ -1,13 +1,14 @@
 import { RailRoute } from '@admin/models/route.model';
 import { RouteStore } from '@admin/store/routes/routes.store';
 import { Component, inject, OnInit, Signal } from '@angular/core';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 import { RouteCardComponent } from '../route-card/route-card.component';
 
 @Component({
   selector: 'app-route-list',
   standalone: true,
-  imports: [RouteCardComponent],
+  imports: [RouteCardComponent, MatProgressSpinner],
   templateUrl: './route-list.component.html',
   styleUrl: './route-list.component.scss',
 })
