12eeabdd9845876d219664b15d5738b982814686
Ruslan Rabadanov <rabadanov.ruslann@gmail.com>
2024-08-23 17:15:32 +0200

    init: start 'Trip details' task

---
+++ src/app/home/pages/trip/components/carriage-list/carriage-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carriage-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './carriage-list.component.html',
+  styleUrl: './carriage-list.component.scss'
+})
+export class CarriageListComponent {
+
+}
--- src/app/home/pages/trip/trip.component.ts
+++ src/app/home/pages/trip/trip.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RoutePath } from '@shared/models/enums/route-path.enum';
 
 @Component({
   selector: 'app-trip',
   standalone: true,
   imports: [],
   templateUrl: './trip.component.html',
-  styleUrl: './trip.component.scss',
+  styleUrls: ['./trip.component.scss'],
 })
-export class TripComponent {}
+export class TripComponent implements OnInit {
+  rideId: string | null = null;
+
+  fromStationId: string | null = null;
+
+  toStationId: string | null = null;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.rideId = this.activatedRoute.snapshot.paramMap.get('rideId');
+    this.fromStationId = this.activatedRoute.snapshot.queryParamMap.get('from');
+    this.toStationId = this.activatedRoute.snapshot.queryParamMap.get('to');
+
+    if (!(this.rideId && this.fromStationId && this.toStationId)) {
+      // Redirect to not found page
+      this.router.navigate([RoutePath.NotFound]);
+    }
+  }
+}
