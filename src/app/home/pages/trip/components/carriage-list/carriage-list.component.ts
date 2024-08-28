ba379862090017a82c7c792390d6765ee03a40d2
Ruslan Rabadanov <rabadanov.ruslann@gmail.com>
2024-08-28 09:58:38 +0200

    fix: do not display cars with no available seats

--- src/app/home/pages/trip/components/carriage-list/carriage-list.component.ts
+++ src/app/home/pages/trip/components/carriage-list/carriage-list.component.ts
@@ -1,13 +1,15 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input, Signal } from '@angular/core';
 import { MatList, MatListItem } from '@angular/material/list';
 import { TripStore } from '@home/store/trip/trip.store';
 import { TrainCarComponent } from '@shared/components/train-car/train-car.component';
 import { Carriage } from '@shared/models/interfaces/carriage.model';
+import { TrainCarService } from '@shared/services/train-car/train-car.service';
 
 @Component({
   selector: 'app-carriage-list',
   standalone: true,
   imports: [MatList, MatListItem, TrainCarComponent],
+  providers: [TrainCarService],
   templateUrl: './carriage-list.component.html',
   styleUrl: './carriage-list.component.scss',
 })
@@ -15,6 +17,16 @@ export class CarriageListComponent {
   carriages = input.required<Carriage[]>();
 
   tripStore = inject(TripStore);
+
+  availableCarriages!: Signal<Carriage[]>;
+
+  constructor(private trainCarService: TrainCarService) {
+    this.availableCarriages = computed(() => {
+      return this.carriages().filter(
+        (carriage) => !!this.trainCarService.getAvailableSeatsNumber(carriage),
+      );
+    });
+  }
 
   onToggleSeat(seatNumber: number, carCode: string) {
     this.tripStore.toggleSeatState(carCode, seatNumber);
