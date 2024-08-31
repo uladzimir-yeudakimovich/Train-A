fc4cc2caa585436f9f6db6377513238cec588656
Ruslan Rabadanov <rabadanov.ruslann@gmail.com>
2024-08-31 19:27:23 +0200

    refactor: cdk virtual scroll for Route list

--- src/app/admin/pages/routes/route-list/route-list.component.ts
+++ src/app/admin/pages/routes/route-list/route-list.component.ts
@@ -1,4 +1,5 @@
 import { RailRoute } from '@admin/models/route.model';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { Component, input } from '@angular/core';
 
 import { RouteCardComponent } from '../route-card/route-card.component';
@@ -6,7 +7,7 @@ import { RouteCardComponent } from '../route-card/route-card.component';
 @Component({
   selector: 'app-route-list',
   standalone: true,
-  imports: [RouteCardComponent],
+  imports: [RouteCardComponent, ScrollingModule],
   templateUrl: './route-list.component.html',
   styleUrl: './route-list.component.scss',
 })
