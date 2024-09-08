b8630ee3d82b8a10e2257f55d90c9924c8656f56
Ruslan Rabadanov <rabadanov.ruslann@gmail.com>
2024-09-08 22:16:58 +0200

    feat: test SignUp page

--- cypress/e2e/authorization.cy.ts
+++ cypress/e2e/authorization.cy.ts
@@ -1,37 +1,68 @@
-import { RoutePath } from "@shared/models/enums/route-path.enum";
+import { RoutePath } from '@shared/models/enums/route-path.enum';
 
 describe('SignIn Page', () => {
-    beforeEach(() => {
-        cy.visit(RoutePath.Login);
-    });
+  beforeEach(() => {
+    cy.visit(RoutePath.Login);
+  });
 
-    it('should have a title with form', () => {
-        cy.contains('Sign In');
-        cy.get('form');
-    });
+  it('should have a title with form', () => {
+    cy.contains('Sign In');
+    cy.get('form');
+  });
 
-    it('should have a form with email and password fields', () => {
-        cy.get('input[type="email"]');
-        cy.get('input[type="password"]');
-    });
+  it('should have a form with email and password fields', () => {
+    cy.get('input[type="email"]');
+    cy.get('input[type="password"]');
+  });
 
-    it('should have a submit button', () => {
-        cy.get('button[type="submit"]').contains('Sign In');
-    });
+  it('should have a submit button', () => {
+    cy.get('button[type="submit"]').contains('Sign In');
+  });
 
-    it('should have a link to Sign Up page', () => {
-        cy.get('a').contains('Sign Up');
-    });
+  it('should have a link to Sign Up page', () => {
+    cy.get('a').contains('Sign Up');
+  });
 
-    it('should navigate to Home after submitting the form', () => {
-        cy.login('admin@admin.com', 'my-password');
-        cy.url().should('eq', Cypress.config().baseUrl + RoutePath.Search);
-    });
+  it('should navigate to Home after submitting the form', () => {
+    cy.login('admin@admin.com', 'my-password');
+    cy.url().should('eq', Cypress.config().baseUrl + RoutePath.Search);
+  });
 
-    it('should show error messages for unsuccessful login', () => {
-        cy.get('input[type="email"]').type('admin@admin.com');
-        cy.get('input[type="password"]').type('invalid-password');
-        cy.get('button[type="submit"]').click();
-        cy.get('mat-error').should('have.length', 2);
-    });
+  it('should show error messages for unsuccessful login', () => {
+    cy.get('input[type="email"]').type('admin@admin.com');
+    cy.get('input[type="password"]').type('invalid-password');
+    cy.get('button[type="submit"]').click();
+    cy.get('mat-error').should('have.length', 2);
+  });
+
+  it('should have navigation to Sign Up page', () => {
+    cy.get('app-login').contains('Sign Up').click();
+    cy.url().should('eq', Cypress.config().baseUrl + RoutePath.Registration);
+  });
+});
+
+describe('SignUp Page', () => {
+  beforeEach(() => {
+    cy.visit(RoutePath.Registration);
+  });
+
+  it('should register a new user and navigate to SignIn', () => {
+    // TODO: fix the mockRegisterUser command
+    const randomEmail =
+      Math.random().toString(36).substring(7) + '@example.com';
+    cy.mockRegisterUser();
+
+    cy.register(randomEmail, 'password');
+    cy.url().should('include', RoutePath.Login);
+  });
+
+  it('should show error messages for unsuccessful registration', () => {
+    cy.register('admin@admin.com', 'password');
+    cy.get('mat-error');
+  });
+
+  it('should have navigation to Sign In page', () => {
+    cy.get('app-registration').contains('Sign In').click();
+    cy.url().should('eq', Cypress.config().baseUrl + RoutePath.Login);
+  });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,9 +1,12 @@
 import { RoutePath } from '@shared/models/enums/route-path.enum';
+import { ApiPath } from '@shared/models/enums/api-path.enum';
 
 declare global {
   namespace Cypress {
     interface Chainable {
       login(email: string, password: string): Chainable<void>;
+      register(email: string, password: string): Chainable<void>;
+      mockRegisterUser(): Chainable<void>;
     }
   }
 }
@@ -13,4 +16,19 @@ Cypress.Commands.add('login', (email: string, password: string) => {
   cy.get('input[type="email"]').type(email);
   cy.get('input[type="password"]').type(password);
   cy.get('button[type="submit"]').click();
+});
+
+Cypress.Commands.add('register', (email: string, password: string) => {
+  cy.visit(RoutePath.Registration);
+  cy.get('input[name="email"]').type(email);
+  cy.get('input[name="password"]').type(password);
+  cy.get('input[name="confirmPassword"]').type(password);
+  cy.get('button[type="submit"]').click();
+});
+
+Cypress.Commands.add('mockRegisterUser', () => {
+  cy.intercept('POST', `/api/${ApiPath.SignUp}`, {
+    statusCode: 201,
+    body: {},
+  }).as('registerUser');
 });
