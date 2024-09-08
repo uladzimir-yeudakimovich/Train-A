1668d94f2a8f64d3537f1a2f0b1773650e4c34d5
Ruslan Rabadanov <rabadanov.ruslann@gmail.com>
2024-09-08 22:55:41 +0200

    feat: test search

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,6 +1,8 @@
+import { RoutePath } from "@shared/models/enums/route-path.enum";
+
 describe('Unauthorized Navigation', () => {
   it('should open Home page', () => {
-    cy.visit('http://localhost:4200/');
+    cy.visit(RoutePath.Search);
     cy.get('app-navigation')
       .get('a')
       .filter('[ng-reflect-activated="true"]')
@@ -8,7 +10,7 @@ describe('Unauthorized Navigation', () => {
   });
 
   it('should open SignIn page', () => {
-    cy.visit('http://localhost:4200/Train-A/#/signin');
+    cy.visit(RoutePath.Login);
     cy.get('app-navigation')
       .get('a')
       .filter('[ng-reflect-activated="true"]')
@@ -16,7 +18,7 @@ describe('Unauthorized Navigation', () => {
   });
 
   it('should open SignUp page', () => {
-    cy.visit('http://localhost:4200/Train-A/#/signup');
+    cy.visit(RoutePath.Registration);
     cy.get('app-navigation')
       .get('a')
       .filter('[ng-reflect-activated="true"]')
@@ -24,7 +26,7 @@ describe('Unauthorized Navigation', () => {
   });
 
   it('should redirect to SignIn page', () => {
-    cy.visit('http://localhost:4200/Train-A/#/admin');
+    cy.visit(RoutePath.Admin);
     cy.get('app-navigation')
       .get('a')
       .filter('[ng-reflect-activated="true"]')
@@ -32,34 +34,34 @@ describe('Unauthorized Navigation', () => {
   });
 
   it('should redirect to Not Found page', () => {
-    cy.visit('http://localhost:4200/Train-A/#/404');
+    cy.visit(RoutePath.NotFound);
     cy.contains('Not Found');
   });
 
   it('should move to SignIn page', () => {
-    cy.visit('http://localhost:4200/');
+    cy.visit(RoutePath.Search);
     cy.get('app-navigation').get('a').contains('Sign In').click();
     cy.url().should('include', '/signin');
   });
 
   it('should move to SignUp page', () => {
-    cy.visit('http://localhost:4200/');
+    cy.visit(RoutePath.Search);
     cy.get('app-navigation').get('a').contains('Sign Up').click();
     cy.url().should('include', '/signup');
   });
 
   it('should not show Admin link', () => {
-    cy.visit('http://localhost:4200/');
+    cy.visit(RoutePath.Search);
     cy.get('app-navigation').get('a').contains('Admin').should('not.exist');
   });
 
   it('should not show Profile link', () => {
-    cy.visit('http://localhost:4200/');
+    cy.visit(RoutePath.Search);
     cy.get('app-navigation').get('a').contains('Profile').should('not.exist');
   });
 
   it('should not show Orders link', () => {
-    cy.visit('http://localhost:4200/');
+    cy.visit(RoutePath.Search);
     cy.get('app-navigation').get('a').contains('Orders').should('not.exist');
   });
 });
