c12f87bd61ed17a84f28e97666af750eec62c0c6
Ruslan Rabadanov <rabadanov.ruslann@gmail.com>
2024-09-09 23:33:24 +0200

    fix: commit validation

--- .validate_commit.sh
+++ .validate_commit.sh
@@ -2,7 +2,7 @@
 
 echo "Checking commit messages..."
 
-BASE_BRANCH=$(git merge-base origin/${GITHUB_BASE_REF} HEAD)
+BASE_BRANCH=$(git merge-base ${GITHUB_BASE_REF} HEAD)
 
 if git log --format="%s" $BASE_BRANCH..HEAD | grep -vE '^(init|feat|fix|docs|chore|refactor)( #[0-9]+)?: .{1,100}'
 then
