571e174165a42179ed07deef55610aa2738c352e
Ruslan Rabadanov <rabadanov.ruslann@gmail.com>
2024-09-10 00:01:20 +0200

    test HURAAAAA

--- .validate_commit.sh
+++ .validate_commit.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-echo "Checking commit messages for ${GITHUB_BASE_REF}"
+# Get the current branch
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Checking commit messages for ${CURRENT_BRANCH} against main"
 
-BASE_BRANCH=$(git merge-base ${GITHUB_BASE_REF} HEAD)
+git fetch origin main:main
+BASE_BRANCH=$(git merge-base main ${CURRENT_BRANCH})
 
-if git log --format="%s" $BASE_BRANCH..HEAD | grep -vE '^(init|feat|fix|docs|chore|refactor)( #[0-9]+)?: .{1,100}'
+if git log --format="%s" $BASE_BRANCH..${CURRENT_BRANCH} | grep -vE '^(init|feat|fix|docs|chore|refactor)( #[0-9]+)?: .{1,100}'
 then
     echo "Invalid commit message found."
     exit 1
