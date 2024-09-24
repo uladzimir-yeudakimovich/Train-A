ecb6c123770d4e6dc4140e71670b084857e0661a
Ruslan Rabadanov <ruslanrabadanov2101@gmail.com>
2024-09-24 14:39:35 +0200

    feat: expansion panel for rides

--- src/app/admin/pages/rides/components/ride-list/ride-list.component.ts
+++ src/app/admin/pages/rides/components/ride-list/ride-list.component.ts
@@ -1,5 +1,7 @@
 import { RidesStore } from '@admin/store/rides/ride.store';
-import { Component, inject } from '@angular/core';
+import { Component, inject, viewChild } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 import { RideCardComponent } from '../ride-card/ride-card.component';
@@ -7,10 +9,17 @@ import { RideCardComponent } from '../ride-card/ride-card.component';
 @Component({
   selector: 'app-ride-list',
   standalone: true,
-  imports: [RideCardComponent, MatProgressSpinner],
+  imports: [
+    RideCardComponent,
+    MatProgressSpinner,
+    MatExpansionModule,
+    MatButton,
+  ],
   templateUrl: './ride-list.component.html',
   styleUrl: './ride-list.component.scss',
 })
 export class RideListComponent {
   store = inject(RidesStore);
+
+  accordion = viewChild.required(MatAccordion);
 }
