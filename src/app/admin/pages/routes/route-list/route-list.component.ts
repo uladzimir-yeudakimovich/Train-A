5b3f0e803c712861bb4a7bde02db86d73c883de5
Ruslan Rabadanov <ruslanrabadanov2101@gmail.com>
2024-09-25 20:13:38 +0200

    feat: paginator for Routes

--- src/app/admin/pages/routes/route-list/route-list.component.ts
+++ src/app/admin/pages/routes/route-list/route-list.component.ts
@@ -1,17 +1,41 @@
 import { RailRoute } from '@admin/models/route.model';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 import { RouteCardComponent } from '../route-card/route-card.component';
 
 @Component({
   selector: 'app-route-list',
   standalone: true,
-  imports: [RouteCardComponent, ScrollingModule],
+  imports: [RouteCardComponent, MatPaginatorModule],
   templateUrl: './route-list.component.html',
   styleUrl: './route-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RouteListComponent {
+export class RouteListComponent implements OnInit {
   routes = input.required<RailRoute[]>();
+
+  pageRoutes = signal<RailRoute[]>([]);
+
+  readonly pageSizeOptions = [10, 20, 50, 100];
+
+  ngOnInit(): void {
+    this.setPageRoutes(0, this.pageSizeOptions[0]);
+  }
+
+  handlePageEvent(e: PageEvent) {
+    this.setPageRoutes(e.pageIndex, e.pageSize);
+  }
+
+  private setPageRoutes(pageIndex: number, pageSize: number): void {
+    const start = pageIndex * pageSize;
+    const end = start + pageSize;
+    this.pageRoutes.set(this.routes().slice(start, end));
+  }
 }
